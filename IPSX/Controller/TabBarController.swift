a60dc41fa52e3a51bf6cfb6cf174b6c5233f3bc2
Cristina Virlan <cristina@ip.sx>
2018-04-18 17:20:26 +0300

    Implemented TabBarController and added assets

---
+++ IPSX/Controller/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  IPSX
+//
+//  Created by Cristina Virlan on 18/04/2018.
+//  Copyright © 2018 Cristina Virlan. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
