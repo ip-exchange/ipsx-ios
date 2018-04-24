12c81c00a6ed0781f3c6b9abba0b9b9240765015
Calin Chitu <calin@ip.sx>
2018-04-24 15:26:31 +0300

    Add tabbar assets, code and storyboard to dev branch

---
+++ IPSX/Controller/DashboardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardViewController.swift
+//  IPSX
+//
+//  Created by Cristina Virlan on 18/04/2018.
+//  Copyright © 2018 Cristina Virlan. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
