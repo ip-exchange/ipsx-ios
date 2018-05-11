7aca91067db2f9fbfa18584029a95d811ae888f4
Calin Chitu <calin@ip.sx>
2018-05-11 16:05:21 +0300

    Add logic to show the actual token request list

--- IPSX/Flows/Dashboard/Controller/TokenRequestListController.swift
+++ IPSX/Flows/Dashboard/Controller/TokenRequestListController.swift
@@ -13,11 +13,10 @@ import UIKit
 class TokenRequestListController: UIViewController {
 
     let cellID = "TokenRequestCellID"
-    var tokenRequests: [TokenRequest]?
+    var tokenRequests: [TokenRequest] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
 }
@@ -25,17 +24,34 @@ class TokenRequestListController: UIViewController {
 extension TokenRequestListController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return tokenRequests.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! TokenRequestCell
+        cell.configure(tokenRequest: tokenRequests[indexPath.item])
         return cell
     }
 }
 
 
 class TokenRequestCell: UITableViewCell {
-    @IBOutlet weak var textlabel: UILabel!
+    
+    @IBOutlet weak var aliasLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var completedView: RoundedView!
+    @IBOutlet weak var pendingView: RoundedView!
+    
+    func configure(tokenRequest: TokenRequest) {
+        //TODO (CC): Get the wallet name
+        //aliasLabel.text = tokenRequest.ethID
+        if let date = tokenRequest.created {
+            dateLabel.text = DateFormatter.dateStringForTokenRequests(date: date)
+        }
+        quantityLabel.text = "Requested: " + tokenRequest.amount
+        pendingView.isHidden   = tokenRequest.status == "pending"
+        completedView.isHidden = tokenRequest.status == "completed"
+    }
 }
