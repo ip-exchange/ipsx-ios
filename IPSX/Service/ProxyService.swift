c0f3ebf9db99630727cfe4b47d0be83aca725ebd
Calin Chitu <calin@ip.sx>
2018-05-11 14:12:42 +0300

    Move date formatter related code to an extension

--- IPSX/Service/ProxyService.swift
+++ IPSX/Service/ProxyService.swift
@@ -38,11 +38,7 @@ class ProxyService {
         
         var proxies: [Proxy] = []
         
-        //TODO (CC): refactor this into an extension !
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        let dateFormatter = DateFormatter.backendResponseParse()
         
         for json in jsonArray {
             
@@ -62,8 +58,8 @@ class ProxyService {
             var remainigDuartionString = "0 min"
             if let eDate = endDate, eDate.timeIntervalSince(Date()) > 0 {
                 let remainingDuration = eDate.timeIntervalSince(Date())
-                let components = secondsToDaysHoursMinutes(seconds: Int(remainingDuration))
-                remainigDuartionString = readableDaysHoursMinutes(components:components)
+                let components = DateFormatter.secondsToDaysHoursMinutes(seconds: Int(remainingDuration))
+                remainigDuartionString = DateFormatter.readableDaysHoursMinutes(components:components)
             }
             
             let country = json["country"].stringValue
@@ -84,24 +80,6 @@ class ProxyService {
         }
         UserManager.shared.storeProxyDetails(proxies: proxies)
         completionHandler(ServiceResult.success(proxies))
-    }
-    
-    //TODO (CC) move this !
-    
-    private func secondsToDaysHoursMinutes (seconds : Int) -> (Int, Int, Int) {
-        return (seconds / 86400, (seconds % 86400) / 3600, (seconds % 3600) / 60)
-    }
-    
-    //TODO (CC) move this !
-    
-    private func readableDaysHoursMinutes (components: (d: Int, h: Int, m: Int)) -> String {
-        var days    = ""
-        var hours   = ""
-        var minutes = "0 min"
-        if components.d > 0 { days    = "\(components.d) d " }
-        if components.h > 0 { hours   = "\(components.h) h " }
-        if components.m > 0 { minutes = "\(components.m) min" }
-        return days+hours+minutes
     }
     
     func getProxyCountryList(completionHandler: @escaping (ServiceResult<Any>) -> ()) {
@@ -177,11 +155,7 @@ class ProxyService {
                 
                 let createdString = json["created_at"].stringValue
                 
-                //TODO (CC) move this !
-                
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-                dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+                let dateFormatter = DateFormatter.backendResponseParse()
                 
                 let createdDate = dateFormatter.date(from: createdString)
                 
