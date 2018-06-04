fa5b83ad5a65a466fe65528bfee2510e8d2142de
Cristina Virlan <cristina@ip.sx>
2018-06-04 16:50:59 +0300

    Implement FB Login

--- IPSX/AppDelegate.swift
+++ IPSX/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,7 +22,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         setKeychainAccessGroup()
         UserManager.shared.retrieveAccessDetails()
         let _ = ReachabilityManager.shared
-        return true
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        FBSDKAppEvents.activateApp()
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
     }
     
     func setKeychainAccessGroup() {
@@ -32,11 +42,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         let keychainGroup = appIdentifierPrefix + bundleId
         KeychainWrapper.accessGroup = keychainGroup
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
--- IPSX/Flows/Login/Controller/LoginOptionsController.swift
+++ IPSX/Flows/Login/Controller/LoginOptionsController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import FacebookLogin
+import FBSDKLoginKit
 
 class LoginOptionsController: UIViewController {
 
     @IBOutlet weak var backgroundImageView: UIImageView!
+    var dict: [String : AnyObject] = [:]
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -18,4 +21,43 @@ class LoginOptionsController: UIViewController {
     }
 
     @IBAction func unwindToLoginOptions(segue:UIStoryboardSegue) { }
+    
+    @IBAction func facebookLoginAction(_ sender: UIButton) {
+        if let accessToken = FBSDKAccessToken.current(){
+            print("Already logged in. Access token:",accessToken)
+            getFBUserData()
+        }
+        else {
+            facebookLogin()
+        }
+    }
+    
+    func facebookLogin() {
+        
+        let loginManager = LoginManager()
+        loginManager.logIn(readPermissions: [.publicProfile], viewController: self, completion: { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let _, let _, let accessToken):
+                print("FB ACCESS TOKEN:", accessToken)
+                self.getFBUserData()
+            }
+        })
+    }
+    
+    //function is fetching the user data
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    print(self.dict)
+                }
+            })
+        }
+    }
+    
 }
