09dbb6e936555b3fc5fafc62629e9f0451f222ff
Cristina Virlan <cristina@ip.sx>
2018-09-21 14:48:33 +0300

    Implement collectionView for Provider feature

--- IPSX/Flows/Dashboard/Controller/Provider/AboutProviderViewController.swift
+++ IPSX/Flows/Dashboard/Controller/Provider/AboutProviderViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AboutProviderViewController: UIViewController {
+class AboutProviderViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBAction func closeAction(_ sender: Any) {
         dismiss(animated: true)
@@ -18,8 +18,75 @@ class AboutProviderViewController: UIViewController {
         print("TODO")
     }
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    let dataSource = [["title" : "Your Job as Provider".localized,
+                      "subtitle" : "Your Job as Provider Text".localized,
+                      "imageName" : "trade"],
+                      
+                      ["title" : "Providers Dashboard".localized,
+                       "subtitle" : "Providers Dashboard Text".localized,
+                       "imageName" : "providerDashboard"],
+                      
+                      ["title" : "Package Builder".localized,
+                       "subtitle" : "Package Builder Text".localized,
+                       "imageName" : "packageBuilder"]]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let collectionViewLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout
+        collectionViewLayout?.minimumLineSpacing = 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        let noOfItems = dataSource.count
+        pageControl.numberOfPages = noOfItems
+        pageControl.isHidden = !(noOfItems > 1)
+        return noOfItems
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AboutProviderCellID", for: indexPath) as! AboutProviderCell
+        if dataSource.count > indexPath.row {
+            let title     = dataSource[indexPath.row]["title"]
+            let subtitle  = dataSource[indexPath.row]["subtitle"]
+            let imageName = dataSource[indexPath.row]["imageName"]
+            cell.configure(title: title, subtitle: subtitle, imageName: imageName)
+        }
+        return cell
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        pageControl?.currentPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
+    }
+    
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        pageControl?.currentPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = self.view.frame.size.width
+        let height = self.view.frame.size.height * 0.52
+        return CGSize(width: width, height: height)
+    }
+}
+
+class AboutProviderCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    func configure(title: String?, subtitle: String?, imageName: String?) {
+        
+        titleLabel?.text = title
+        subtitleLabel?.text = subtitle
+        imageView?.image = UIImage(named: imageName ?? "trade")
     }
 }
