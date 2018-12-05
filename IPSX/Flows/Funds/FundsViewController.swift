663d94b46bc5c14af026cf8f558ca764dc99e31e
Calin Chitu <calin@ip.sx>
2018-12-05 10:01:42 +0200

    Complete Funds main screen

--- IPSX/Flows/Funds/FundsViewController.swift
+++ IPSX/Flows/Funds/FundsViewController.swift
@@ -11,26 +11,29 @@ import IPSXNetworkingFramework
 
 class FundsViewController: UIViewController {
 
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var titleTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var hidableDownView: UIView!
+    @IBOutlet weak var topRootView: UIView!
     @IBOutlet weak var loadingView: CustomLoadingView!
-    @IBOutlet weak var tokensAmountLabel: UILabel!
-    @IBOutlet weak var separatorView: UIView!
-    @IBOutlet weak var topBarView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var amountLargeLabel: UILabel!
+    @IBOutlet weak var amountTopSmallLabel: UILabel!
+    
+    let maxHeaderHeight: CGFloat = 215;
+    let minHeaderHeight: CGFloat = 44;
+    var previousScrollOffset: CGFloat = 0;
+    var toast: ToastAlertView?
+    var topConstraint: NSLayoutConstraint?
+    var userInfo: UserInfo? { return UserManager.shared.userInfo }
+    
     @IBOutlet weak var topConstraintOutlet: NSLayoutConstraint! {
         didSet {
             topConstraint = topConstraintOutlet
         }
     }
-    var toast: ToastAlertView?
-    var topConstraint: NSLayoutConstraint?
     
-    var balance: String = "" {
-        didSet {
-            DispatchQueue.main.async {
-                self.tokensAmountLabel.text = self.balance
-            }
-        }
-    }
-
+    
     var errorMessage: String? {
         didSet {
             if ReachabilityManager.shared.isReachable() {
@@ -38,26 +41,206 @@ class FundsViewController: UIViewController {
             }
         }
     }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        balance = UserManager.shared.userInfo?.balance?.cleanString ?? "0"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        updateHeader()
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(appWillEnterForeground),
+                                               name: UIApplication.willEnterForegroundNotification,
+                                               object: nil)
+    }
+    
+    @objc func appWillEnterForeground() {
+        updateReachabilityInfo()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        createToastAlert(onTopOf: separatorView, text: "")
+        createToastAlert(onTopOf: topRootView, text: "")
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(reachabilityChanged(_:)), name: ReachabilityChangedNotification, object: nil)
+        updateReachabilityInfo()
+        refreshProfileUI()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        topRootView.createParticlesAnimation()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        topRootView.removeParticlesAnimation()
+        NotificationCenter.default.removeObserver(self, name: ReachabilityChangedNotification, object: nil)
+    }
+    
+    @objc public func reachabilityChanged(_ note: Notification) {
+        DispatchQueue.main.async {
+            let reachability = note.object as! Reachability
+            
+            if !reachability.isReachable {
+                self.toast?.showToastAlert("No internet connection".localized, dismissable: false)
+            } else if self.toast?.currentText == "No internet connection".localized {
+                self.toast?.hideToastAlert()
+            }
+        }
+    }
+    
+    func updateReachabilityInfo() {
+        DispatchQueue.main.async {
+            if !ReachabilityManager.shared.isReachable() {
+                self.toast?.showToastAlert("No internet connection".localized, dismissable: false)
+            } else if self.toast?.currentText == "No internet connection".localized {
+                self.toast?.hideToastAlert()
+            }
+        }
+    }
+    
+    func updateHeader() {
+        let range = self.maxHeaderHeight - self.minHeaderHeight
+        let openAmount = self.headerHeightConstraint.constant - self.minHeaderHeight
+        let percentage = openAmount / range
+        
+        self.titleTopConstraint.constant = -openAmount + 10
+        self.hidableDownView.alpha = percentage
+    }
+    
+    private func configureUI() {
+        self.headerHeightConstraint.constant = self.maxHeaderHeight
+        tableView.tableFooterView = UIView()
+    }
+    
+    func refreshProfileUI() {
+        
+        DispatchQueue.main.async {
+            //TODO: Get here the amount to display
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        switch segue.identifier {
+            
+            
+        default:
+            break
+        }
+    }
+    
+    func retrieveUserInfo() {
+        
+        loadingView?.startAnimating()
+        UserInfoService().retrieveUserInfo(completionHandler: { result in
+            
+            self.loadingView?.stopAnimating()
+            switch result {
+            case .success(let user):
+                UserManager.shared.userInfo = user as? UserInfo
+                self.refreshProfileUI()
+                
+            case .failure(let error):
+                self.handleError(error, requestType: RequestType.userInfo, completion: {
+                    self.retrieveUserInfo()
+                })
+            }
+        })
+    }
+    
+    func collapseHeader() {
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.2, animations: {
+            self.headerHeightConstraint.constant = self.minHeaderHeight
+            self.updateHeader()
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func expandHeader() {
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.2, animations: {
+            self.headerHeightConstraint.constant = self.maxHeaderHeight
+            self.updateHeader()
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func setScrollPosition(_ position: CGFloat) {
+        self.tableView.contentOffset = CGPoint(x: self.tableView.contentOffset.x, y: position)
+    }
+    
 }
 
+
+extension FundsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension FundsViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let scrollDiff = scrollView.contentOffset.y - self.previousScrollOffset
+        
+        let isScrollingDown =  scrollView.contentOffset.y > 0
+        let isScrollingUp = scrollView.contentOffset.y < 0
+        
+        var newHeight = self.headerHeightConstraint.constant
+        if isScrollingDown {
+            newHeight = max(self.minHeaderHeight, self.headerHeightConstraint.constant - abs(scrollDiff))
+        } else if isScrollingUp {
+            newHeight = min(self.maxHeaderHeight, self.headerHeightConstraint.constant + abs(scrollDiff))
+        }
+        
+        if newHeight != self.headerHeightConstraint.constant {
+            self.headerHeightConstraint.constant = newHeight
+            self.updateHeader()
+            self.setScrollPosition(self.previousScrollOffset)
+        }
+        
+        self.previousScrollOffset = scrollView.contentOffset.y
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        self.scrollViewDidStopScrolling()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            self.scrollViewDidStopScrolling()
+        }
+    }
+    
+    func scrollViewDidStopScrolling() {
+        let range = self.maxHeaderHeight - self.minHeaderHeight
+        let midPoint = self.minHeaderHeight + (range / 2)
+        
+        if self.headerHeightConstraint.constant > midPoint {
+            self.expandHeader()
+        } else {
+            self.collapseHeader()
+        }
+    }
+}
 
 extension FundsViewController: ToastAlertViewPresentable {
     
     func createToastAlert(onTopOf parentUnderView: UIView, text: String) {
         if self.toast == nil, let toastView = ToastAlertView(parentUnderView: parentUnderView, parentUnderViewConstraint: self.topConstraint!, alertText:text) {
             self.toast = toastView
-            view.insertSubview(toastView, belowSubview: topBarView)
+            view.addSubview(toastView)
         }
     }
 }
@@ -68,9 +251,25 @@ extension FundsViewController: ErrorPresentable {
         
         switch error {
             
-         default:
-            self.errorMessage = "Generic Error Message".localized
+        case CustomError.expiredToken:
+            
+            LoginService().getNewAccessToken(errorHandler: { error in
+                self.errorMessage = "Generic Error Message".localized
+                
+            }, successHandler: {
+                completion?()
+            })
+            
+        default:
+            
+            switch requestType {
+            case RequestType.userInfo, RequestType.getEthAddress:
+                self.errorMessage = "Refresh Data Error Message".localized
+            case RequestType.deleteEthAddress:
+                self.errorMessage = "ETH Address Delete Failed Error Message".localized
+            default:
+                self.errorMessage = "Generic Error Message".localized
+            }
         }
-        
     }
 }
